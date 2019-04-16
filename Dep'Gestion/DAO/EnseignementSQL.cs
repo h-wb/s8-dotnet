57a6c765f7fb18e2825eb2a5e45445efeb3acee9
anthonyG57 <32765356+anthonyG57@users.noreply.github.com>
2019-04-16 21:37:44 +0200

    oui

---
+++ Dep'Gestion/DAO/EnseignementSQL.cs
@@ -0,0 +1,124 @@
+﻿using DAO;
+using Metier;
+using Outils;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Dep_Gestion.DAO
+{
+    class EnseignementSQL : DAO<Enseignement>
+    {
+        public override Enseignement create(Enseignement obj)
+        {
+            Enseignement tc = null;
+            tc = this.find(obj.nom);
+
+            Console.WriteLine(tc);
+            // Console.ReadLine();
+
+            if (tc != null)
+            {
+                return tc;
+            }
+            else
+            {
+                obj.id = OutilsSQL.getLastInsertedId("enseignement", Connexion.getInstance()) + 1;
+                using (SqlCommand command_c = new SqlCommand("INSERT INTO enseignement VALUES (" + obj.id + ", '" + obj.nom + "');", Connexion.getInstance()))
+                {
+                    command_c.ExecuteNonQuery();
+                    return obj;
+                }
+            }
+        }
+
+        public override void delete(Enseignement obj)
+        {
+            Debug.WriteLine(obj.id);
+            using (SqlCommand command = new SqlCommand("DELETE FROM enseignement WHERE id=" + obj.id + ";", Connexion.getInstance()))
+            {
+                command.ExecuteNonQuery();
+            }
+
+
+            Connexion.getInstance().Close();
+            Console.ReadLine();
+        }
+
+        public override Enseignement find(int id)
+        {
+            Enseignement ens = null;
+            
+            string nom = "";
+
+            using (SqlCommand command_f = new SqlCommand("SELECT nom FROM enseignement WHERE id=" + id + ";", Connexion.getInstance()))
+            {
+                using (SqlDataReader reader_f = command_f.ExecuteReader())
+                {
+                    if (reader_f.HasRows)
+                    {
+                        while (reader_f.Read())
+                        {
+                            nom = reader_f.GetString(0);
+                            ens = new Enseignement(id, nom);
+
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("Aucun objet avec cet id n'a été trouvé.");
+                    }
+
+                    reader_f.Close();
+                }
+
+            }
+
+            Connexion.getInstance().Close();
+
+
+            return ens;
+        }
+
+        public override Enseignement find(string nom)
+        {
+            Enseignement ens = null;
+
+            using (SqlCommand command_f = new SqlCommand("SELECT id, nom FROM enseignement WHERE nom='" + nom + "';", Connexion.getInstance()))
+            {
+                using (SqlDataReader reader_f = command_f.ExecuteReader())
+                {
+                    if (reader_f.HasRows)
+                    {
+                        while (reader_f.Read())
+                        {
+                            ens = new Enseignement(reader_f.GetInt32(0), reader_f.GetString(1));
+                        }
+                    }
+
+                    reader_f.Close();
+                }
+                Connexion.getInstance().Close();
+                return ens;
+
+            }
+        }
+
+        public override Enseignement update(Enseignement objAupdate, Enseignement update)
+        {
+            
+           using (SqlCommand command_u = new SqlCommand(@"UPDATE enseignement SET nom='" + update.nom + "' WHERE id=" + objAupdate.id + ";", Connexion.getInstance()))
+           {
+                command_u.ExecuteNonQuery();
+           }
+
+           Connexion.getInstance().Close();
+
+            return update;
+        }
+    }
+}
---
+++ Dep'Gestion/DAO/PartieAnneeSQL.cs
@@ -0,0 +1,58 @@
+﻿using DAO;
+using Metier;
+using Outils;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dep_Gestion.DAO
+{
+    class PartieAnneeSQL : DAO<PartieAnnee>
+    {
+        public override PartieAnnee create(PartieAnnee obj)
+        {
+            PartieAnnee tc = null;
+            tc = this.find(obj.nom);
+
+            Console.WriteLine(tc);
+            // Console.ReadLine();
+
+            if (tc != null)
+            {
+                return tc;
+            }
+            else
+            {
+                obj.id = OutilsSQL.getLastInsertedId("partie_annee", Connexion.getInstance()) + 1;
+                using (SqlCommand command_c = new SqlCommand("INSERT INTO partie_annee VALUES (" + obj.id + ", '" + obj.nom + "', '" + obj.heuresATravailler + "');", Connexion.getInstance()))
+                {
+                    command_c.ExecuteNonQuery();
+                    return obj;
+                }
+            }
+        }
+
+        public override void delete(PartieAnnee obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override PartieAnnee find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override PartieAnnee find(string nom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override PartieAnnee update(PartieAnnee objAupdate, PartieAnnee update)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
