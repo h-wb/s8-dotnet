ee8e9de74dc1f55368404adace84b8df372ed1d7
hugowehbe <skylinkbox@gmail.com>
2019-04-23 16:14:42 +0200

    gitignore

--- Dep'Gestion/bin/x64/Debug/AppX/Data/SQL/dbo.categorie_enseignant.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[categorie_enseignant] (
-    [id]                  INT IDENTITY (1, 1) NOT NULL,
-    [id_eqtd]             INT NOT NULL,
-    [id_enseignant]       INT NOT NULL,
-    [heures_a_travailler] INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([id] ASC)
-);
-
